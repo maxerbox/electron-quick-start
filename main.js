74e5b6820e8d04e3483100f1af3c3564d9aa4780
mxb <mxb@mxb.com>
2019-05-03 19:58:12 +0200

    5.0 support and version displaying

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      sandbox: false,
+      webviewTag: true
     }
   })
 
